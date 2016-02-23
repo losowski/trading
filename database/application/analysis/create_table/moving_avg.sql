5f7b36f7e30832a38a97cbde647743b651bc7f46
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-02-23 21:21:11 +0000

    Averages working. Cloned from the diff files. Methodology is exactly the same

--- database/application/analysis/create_table/moving_avg.sql
+++ database/application/analysis/create_table/moving_avg.sql
@@ -1,31 +1,20 @@
-CREATE SEQUENCE trading_schema.moving_avg_id_seq
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-ALTER TABLE trading_schema.moving_average_id_seq
-  OWNER TO trading;
-
-
 CREATE TABLE trading_schema.a_moving_avg
 (
-	id bigint NOT NULL DEFAULT nextval('trading_schema.moving_avg_id_seq'::regclass),
-	quote_id bigint NOT NULL,
-	day2 money NOT NULL,
-	days5 money NOT NULL,
-	days9 money NOT NULL,
-	days15 money NOT NULL,
-	days21 money NOT NULL,
-	days29 money NOT NULL,
-	days73 money NOT NULL,
-	days91 money NOT NULL,
-	days121 money NOT NULL,
-	days189 money NOT NULL,
+	id bigint NOT NULL,
+	days2 numeric NOT NULL,
+	days5 numeric NOT NULL,
+	days9 numeric NOT NULL,
+	days15 numeric NOT NULL,
+	days21 numeric NOT NULL,
+	days29 numeric NOT NULL,
+	days73 numeric NOT NULL,
+	days91 numeric NOT NULL,
+	days121 numeric NOT NULL,
+	days189 numeric NOT NULL,
 	CONSTRAINT pk_moving_avg PRIMARY KEY (id ),
-	CONSTRAINT fk_moving_avg_1 FOREIGN KEY (quote_id)
-			REFERENCES trading_schema.quote (id) MATCH SIMPLE
-			ON UPDATE NO ACTION ON DELETE NO ACTION
+	CONSTRAINT fk_moving_avg FOREIGN KEY (id)
+		REFERENCES trading_schema.quote (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
 	OIDS=FALSE
--- database/application/analysis/moving_avg.sql
+++ database/application/analysis/moving_avg.sql
@@ -4,7 +4,7 @@
 CREATE OR REPLACE FUNCTION trading_schema.pCalcAvg(
 	p_symbol						trading_schema.symbol.name%TYPE,
 	p_datestamp						trading_schema.quote.datestamp%TYPE,
-	p_inverval						interval
+	p_interval						interval
 	) RETURNS money AS $$
 DECLARE
 	v_value							money;
@@ -15,7 +15,7 @@ BEGIN
 		v_value
 	FROM
 		trading_schema.quote q
-		INNER JOIN trading_schema.symbol s ON (s.id = q.symbol_id)	
+		INNER JOIN trading_schema.symbol s ON (s.id = q.symbol_id)
 	WHERE
 		s.symbol = p_symbol
 	AND
@@ -30,7 +30,9 @@ $$ LANGUAGE plpgsql;
 -- Base function to calculate one moving average
 CREATE OR REPLACE FUNCTION trading_schema.pInsCalcMovingAvg(
 	p_symbol						trading_schema.symbol.name%TYPE,
-	p_datestamp						trading_schema.quote.datestamp%TYPE
+	p_quote_id						trading_schema.quote.id%TYPE,
+	p_datestamp						trading_schema.quote.datestamp%TYPE,
+	p_average						trading_schema.a_moving_diff.id%TYPE
 	) RETURNS void AS $$
 DECLARE
 	v_2_days						money;
@@ -44,46 +46,52 @@ DECLARE
 	v_121_days						money;
 	v_189_days						money;
 BEGIN
-	-- Calcualte values
-	SELECT * INTO v_2_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '1 day');
-	SELECT * INTO v_5_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '5 days');
-	SELECT * INTO v_9_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '9 days');
-	SELECT * INTO v_15_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '15 days');
-	SELECT * INTO v_21_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '21 days');
-	SELECT * INTO v_29_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '29 days');
-	SELECT * INTO v_73_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '73 days');
-	SELECT * INTO v_91_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '91 days');
-	SELECT * INTO v_121_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '121 days');
-	SELECT * INTO v_189_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '189 days');
-	-- Push data
-	INSERT INTO
-		a_moving_avg
-	(
-		days2,
-		days5,
-		days9,
-		days15,
-		days21,
-		days29,
-		days73,
-		days91,
-		days121,
-		days189
-	)
-	VALUES
-	(
-		v_2_days,
-		v_5_days,
-		v_9_days,
-		v_15_days,
-		v_21_days,
-		v_29_days,
-		v_73_days,
-		v_91_days,
-		v_121_days,
-		v_189_days
-	)
-	;
+	IF p_average IS NULL THEN
+		-- Calculate values
+		SELECT * INTO v_2_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '1 day');
+		SELECT * INTO v_5_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '5 days');
+		SELECT * INTO v_9_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '9 days');
+		SELECT * INTO v_15_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '15 days');
+		SELECT * INTO v_21_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '21 days');
+		SELECT * INTO v_29_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '29 days');
+		SELECT * INTO v_73_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '73 days');
+		SELECT * INTO v_91_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '91 days');
+		SELECT * INTO v_121_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '121 days');
+		SELECT * INTO v_189_days FROM trading_schema.pCalcAvg(p_symbol, p_datestamp, interval '189 days');
+		IF v_2_days IS NOT NULL AND v_5_days IS NOT NULL AND v_9_days IS NOT NULL AND v_15_days IS NOT NULL AND v_21_days IS NOT NULL AND v_29_days IS NOT NULL AND v_73_days IS NOT NULL AND v_91_days IS NOT NULL AND v_121_days IS NOT NULL AND v_189_days IS NOT NULL THEN
+		-- Push data
+			INSERT INTO
+				trading_schema.a_moving_avg
+			(
+				id,
+				days2,
+				days5,
+				days9,
+				days15,
+				days21,
+				days29,
+				days73,
+				days91,
+				days121,
+				days189
+			)
+			VALUES
+			(
+				p_quote_id,
+				v_2_days,
+				v_5_days,
+				v_9_days,
+				v_15_days,
+				v_21_days,
+				v_29_days,
+				v_73_days,
+				v_91_days,
+				v_121_days,
+				v_189_days
+			)
+			;
+		END IF;
+	END IF;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -93,23 +101,44 @@ CREATE OR REPLACE FUNCTION trading_schema.pCalcSymbolAvg(
 	) RETURNS void AS $$
 DECLARE
 	v_quote							RECORD;
+	v_begin_date					trading_schema.quote.datestamp%TYPE;
 BEGIN
+	SELECT
+		min(q.datestamp)
+	INTO
+		v_begin_date
+	FROM
+		trading_schema.quote q
+		INNER JOIN trading_schema.symbol s ON (q.symbol_id = s.id)
+		LEFT OUTER JOIN trading_schema.a_moving_diff a_diff ON (q.id = a_diff.id)
+	WHERE
+		s.symbol = p_symbol
+	AND
+		a_diff.id IS NULL
+	;
+	-- Now loop over the results - so we can progress the records
 	FOR v_quote IN
 		SELECT
-			q.datestamp
+			q.datestamp,
+			q.id,
+			a_avg.id AS quote_id
 		FROM
 			trading_schema.symbol s
 			INNER JOIN trading_schema.quote q ON (s.id = q.symbol_id )
-			LEFT OUTER JOIN trading_schema.a_moving_avg a_avg ON (q.id = a_avg.quote_id)
+			LEFT OUTER JOIN trading_schema.a_moving_avg a_avg ON (q.id = a_avg.id)
 		WHERE
-			s.name = p_symbol
+			s.symbol = p_symbol
 		AND
-			a_avg.quote_id IS NOT NULL
+			q.datestamp >= v_begin_date
 		ORDER BY
 			q.datestamp ASC
 	LOOP
-		PERFORM trading_schema.pInsCalcMovingAvg(p_symbol, v_quote.datestamp);
-		COMMIT;
+		BEGIN
+			PERFORM trading_schema.pInsCalcMovingAvg(p_symbol, v_quote.id, v_quote.datestamp, v_quote.quote_id);
+		EXCEPTION
+			WHEN no_data_found THEN
+				RAISE INFO 'Symbol did not require update';
+		END;
 	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
