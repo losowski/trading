e81ee1eeae05426e701b1fc954222a20fa0691a5
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-02-24 23:58:11 +0000

    Added data compression to the backup. Removed the tar file from the repository

--- database/dump_database.sh
+++ database/dump_database.sh
@@ -6,3 +6,5 @@
 pg_dump -svc --host=localhost -Utrading tradingdb > sql/tradingdb.sql
 #Script to store data only
 pg_dump -va --host=localhost -Utrading tradingdb > sql/tradingdb_data.sql
+#Compress the data
+tar -cvf sql/tradingdb.tar sql/tradingdb.sql sql/tradingdb_data.sql
