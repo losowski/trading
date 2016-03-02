256dac4ba189eb435175a90e7091c20ce0ddd9d6
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-03-02 22:36:22 +0000

    Analysis backbone is nearing completion. Logic inplace to allow database based inquiry Logging helps debug issues

--- python/analysis/analysis_application.py
+++ python/analysis/analysis_application.py
@@ -11,6 +11,7 @@ class AnalysisApplication:
 
 	def __init__(self):
 		self.database		= db_connection.DBConnection()
+		self.symbols_list	= list()
 
 	def __del__(self):
 		pass
@@ -20,7 +21,7 @@ class AnalysisApplication:
 
 	def run(self):
 		self.perform_data_analysis()
-		self.generate_predictions()
+		self.process_trading_flags()
 
 	def perform_data_analysis(self):
 		logging.info("Performing Analysis")
@@ -29,6 +30,8 @@ class AnalysisApplication:
 		symbols_list = analysis_data_query.fetchall()
 		for symbol in symbols_list:
 			logging.debug("Symbol: %s", symbol[0])
+			#Add the symbols to make life easier later on
+			self.symbols_list.append(symbol[0])
 			data_parameters = collections.OrderedDict()
 			data_parameters['symbol'] = symbol[0]
 			data_list = list(data_parameters.values())
@@ -37,17 +40,31 @@ class AnalysisApplication:
 			self.database.commit()
 		analysis_data_query.close()
 
-	def generate_predictions(self):
-		logging.info("Generating Predictions")
-		analysis_data_query = self.database.get_query()
-		analysis_data_query.execute(analysis_queries.get_list_of_symbols)
-		symbols_list = analysis_data_query.fetchall()
-		for symbol in symbols_list:
-			logging.debug("Symbol: %s", symbol[0])
-			data_parameters = collections.OrderedDict()
-			data_parameters['symbol'] = symbol[0]
-			data_list = list(data_parameters.values())
-			#analysis_data_query.callproc(analysis_queries.generate_analysis_table, data_list)
-			self.database.commit()
-		analysis_data_query.close()
-
+	def process_trading_flags(self):
+		logging.info("Process the trading flags")
+		logging.debug("Get the analysis properties")
+		analysis_properties_query = self.database.get_query()
+		analysis_properties_query.execute(analysis_queries.get_analysis_properties)
+		analysis_properties_list = analysis_properties_query.fetchall()
+		for analysis_property in analysis_properties_list:
+			logging.debug("Analysis property %s", analysis_property)
+			analysis_property_id, analysis_property_name, analysis_property_type, analysis_property_assigned_value = analysis_property
+			logging.debug("Property: %s : %s - %s", analysis_property_id, analysis_property_name, analysis_property_type)
+			logging.info("Get the analysis conditions for %s : %s", analysis_property_id, analysis_property_name)
+			property_data_parameters = collections.OrderedDict()
+			property_data_parameters['property_id'] = analysis_property_id
+			logging.debug("property_data_parameters = %s", property_data_parameters)
+			analysis_property_conditions_query = self.database.get_query()
+			analysis_property_conditions_query.execute(analysis_queries.get_analysis_conditions, property_data_parameters)
+			analysis_property_conditions_list = analysis_property_conditions_query.fetchall()
+			for analysis_condition in analysis_property_conditions_list:
+				logging.debug("Conditions: %s", analysis_condition)
+				field_name, operator, threshold_type, duration, value = analysis_condition
+				logging.debug("%s %s %s %s WHEN datestamp is %s", field_name, operator, threshold_type, value, duration)
+				logging.info("Building the SQL statement that runs on the data")
+				for symbol in self.symbols_list:
+					logging.info("Running query on %s", symbol)
+			logging.debug("Closing analysis_conditions_query")
+			analysis_property_conditions_query.close()
+		logging.debug("Closing analysis_properties_query")
+		analysis_properties_query.close()
--- python/analysis/analysis_queries.py
+++ python/analysis/analysis_queries.py
@@ -22,9 +22,59 @@ get_data_analysis = """
 	FROM
 		trading_schema.symbol s
 		INNER JOIN trading_schema.quote q ON (s.id = q.symbol_id)
-		INNER JOIN a_moving_avg amavg ON (amavg.quote_id = q.id)
-		INNER JOIN a_moving_diff amdiff ON (amdiff.id = q.id)
+		LEFT OUTER JOIN trading_schema.quote_diff qd ON (qd.quote_id = q.id)
+		LEFT OUTER JOIN trading_schema.a_moving_avg amavg ON (amavg.id = q.id)
+		LEFT OUTER JOIN trading_schema.a_moving_diff amdiff ON (amdiff.id = q.id)
 	WHERE
 		s.name = %(symbol.name)s
 	;
+"""
+
+# backbone for the logic that will allow DB based analysis
+get_analysis_properties = """
+	SELECT
+		id,
+		name,
+		analysis_type,
+		assigned_value
+	FROM
+		trading_schema.analysis_properties
+	;
+"""
+
+get_analysis_conditions = """
+	SELECT
+		c.field_name,
+		c.operator,
+		c.threshold_type,
+		c.duration,
+		c.value
+	FROM
+		trading_schema.analysis_properties p
+		INNER JOIN trading_schema.analysis_conditions c ON (c.analysis_property_id = p.id)
+	WHERE
+		p.id = %(property_id)s
+	;
+"""
+
+analysis_backbone_query = """
+	SELECT
+		*
+	FROM
+	(
+		SELECT
+			*
+		FROM
+			trading_schema.symbol s
+			INNER JOIN trading_schema.quote q ON (s.id = q.symbol_id)
+			LEFT OUTER JOIN trading_schema.quote_diff qd ON (qd.quote_id = q.id)
+			LEFT OUTER JOIN trading_schema.a_moving_avg amavg ON (amavg.id = q.id)
+			LEFT OUTER JOIN trading_schema.a_moving_diff amdiff ON (amdiff.id = q.id)
+		WHERE
+			s.symbol = %(symbol)s
+	)
+	WHERE
+		%(analysis_conditions)s
+	ORDER BY
+		symbol_datestamp
 """
