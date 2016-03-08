5875fa3175eaf5354ab9f0e797ff888018ba8eb8
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-03-08 22:45:21 +0000

    Added the working structure for the analysis queries. All written for a combination of python SQL creation and the necessary postgresql rummaging

--- python/analysis/analysis_queries.py
+++ python/analysis/analysis_queries.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+#postgres %(name_of_field)s
+#python ${name_of_field}
 
 #Get symbols list
 get_list_of_symbols = """
@@ -56,25 +58,114 @@ get_analysis_conditions = """
 		p.id = %(property_id)s
 	;
 """
+#Only using Python string replace
+analysis_absolute_parameter_template = """
+AND
+	${field_name} ${operator} ${value}
+"""
+#INNER JOIN symbol_quote sq1 ON (sq1.q_datestamp <= sa.q_datestamp AND sq1.q_datestamp >= sa.q_datestamp - '7 DAYS'::interval AND sq1.qd_diff_high_low >= sa.qd_diff_high_low + 0.2 )
+analysis_relative_parameter_template = """
+	INNER JOIN
+		symbol_quote ${relative_index} ON (${relative_index}.q_datestamp <= sa.q_datestamp AND ${relative_index}.q_datestamp >= sa.q_datestamp - ${duration}::interval AND ${relative_index}.${field_name}>= sa.${field_name} + ${value}
+"""
 
 analysis_backbone_query = """
+	WITH symbol_quote AS (
 	SELECT
-		*
+		q.id					AS	q_id,
+		q.datestamp				AS	q_datestamp,
+		q.volume				AS	q_volume,
+		q.adjusted_close_price	AS	q_adjusted_close_price,
+		q.open_price			AS	q_open_price,
+		q.close_price			AS	q_close_price,
+		q.high_price			AS	q_high_price,
+		q.low_price				AS	q_low_price,
+		qd.diff_close_price		AS	qd_diff_close_price,
+		qd.diff_high_price		AS	qd_diff_high_price,
+		qd.diff_low_price		AS	qd_diff_low_price,
+		qd.diff_open_price		AS	qd_diff_open_price,
+		qd.diff_open_close		AS	qd_diff_open_close,
+		qd.diff_high_low		AS	qd_diff_high_low,
+		amavg.days2 			AS	amavg_days2,
+		amavg.days5 			AS	amavg_days5,
+		amavg.days9 			AS	amavg_days9,
+		amavg.days15 			AS	amavg_days15,
+		amavg.days21			AS	amavg_days21,
+		amavg.days29 			AS	amavg_days29,
+		amavg.days73 			AS	amavg_days73,
+		amavg.days91 			AS	amavg_days91,
+		amavg.days121 			AS	amavg_days121,
+		amavg.days189 			AS	amavg_days189,
+		amdiff.days2 			AS	amdiff_days2,
+		amdiff.days5 			AS	amdiff_days5,
+		amdiff.days9 			AS	amdiff_days9,
+		amdiff.days15 			AS	amdiff_days15,
+		amdiff.days21			AS	amdiff_days21,
+		amdiff.days29 			AS	amdiff_days29,
+		amdiff.days73 			AS	amdiff_days73,
+		amdiff.days91 			AS	amdiff_days91,
+		amdiff.days121 			AS	amdiff_days121,
+		amdiff.days189 			AS	amdiff_days189
 	FROM
+		trading_schema.symbol s
+		INNER JOIN trading_schema.quote q ON (s.id = q.symbol_id)
+		LEFT OUTER  JOIN trading_schema.quote_diff qd ON (qd.quote_id = q.id)
+		LEFT OUTER JOIN trading_schema.a_moving_avg amavg ON (q.id = amavg.id)
+		LEFT OUTER JOIN trading_schema.a_moving_diff amdiff ON (q.id = amdiff.id)
+	WHERE
+		s.symbol = %(symbol)s
+	),
+	symbol_absolute AS
 	(
 		SELECT
-			*
+			q_id,
+			q_datestamp,
+			q_volume,
+			q_adjusted_close_price,
+			q_open_price,
+			q_close_price,
+			q_high_price,
+			q_low_price,
+			qd_diff_close_price,
+			qd_diff_high_price,
+			qd_diff_low_price,
+			qd_diff_open_price,
+			qd_diff_open_close,
+			qd_diff_high_low,
+			amavg_days2,
+			amavg_days5,
+			amavg_days9,
+			amavg_days15,
+			amavg_days21,
+			amavg_days29,
+			amavg_days73,
+			amavg_days91,
+			amavg_days121,
+			amavg_days189,
+			amdiff_days2,
+			amdiff_days5,
+			amdiff_days9,
+			amdiff_days15,
+			amdiff_days21,
+			amdiff_days29,
+			amdiff_days73,
+			amdiff_days91,
+			amdiff_days121,
+			amdiff_days189
 		FROM
-			trading_schema.symbol s
-			INNER JOIN trading_schema.quote q ON (s.id = q.symbol_id)
-			LEFT OUTER JOIN trading_schema.quote_diff qd ON (qd.quote_id = q.id)
-			LEFT OUTER JOIN trading_schema.a_moving_avg amavg ON (amavg.id = q.id)
-			LEFT OUTER JOIN trading_schema.a_moving_diff amdiff ON (amdiff.id = q.id)
+			symbol_quote
 		WHERE
-			s.symbol = %(symbol)s
+			TRUE
+			-- Absolute comparators
+			${absolute_comparators}
 	)
-	WHERE
-		%(analysis_conditions)s
-	ORDER BY
-		symbol_datestamp
+SELECT
+	sa.*
+FROM
+	symbol_absolute sa
+	-- Relative Comparators
+	${relative_comparators}
+ORDER BY
+	q_id
+;
 """
