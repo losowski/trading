c69e3d04e36cd38236f561e50aa1be83b81fab2f
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-03-23 23:08:31 +0000

    Cleaned up the WX style of coding

--- python/gui/main_application.py
+++ python/gui/main_application.py
@@ -11,4 +11,5 @@ from database import db_connection
 class MainApplicationWindow(wx.Frame):
 	def __init__(self):
 		wx.Frame.__init__(self, None, size=(1200,800), name="Stock Analysis User Interface")
+		self.Show()
 		pass #__init__ functions
