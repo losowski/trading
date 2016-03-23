60c6a37edb0d9a8d93b311eb465f61958bfd0f58
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-03-23 22:22:12 +0000

    Broke down the entire analysis class into separate functions for ease of GUI development

--- python/analysis/analysis_application.py
+++ python/analysis/analysis_application.py
@@ -95,19 +95,18 @@ class AnalysisApplication(symbols.Symbols, utilities.Utilities):
 		if  len_absolute > 0:
 			absolute = " ".join(absolute_parameters)
 			logging.debug("Analysis absolute: %s", absolute)
-
+		#Build relative parameters
 		relative = str()
 		len_relative = len(relative_parameters)
 		if len(relative_parameters) > 0:
 			relative = " ".join(relative_parameters)
 			logging.debug("Analysis relative: %s", relative)
-
 		relative_join = str()
 		len_relative_join = len(relative_join_parameters)
 		if len(relative_join_parameters) > 0:
 			relative_join = " ".join(relative_join_parameters)
 			logging.debug("Analysis relative join: %s", relative_join)
-
+		#Build hte complete replace functions
 		data = 	{
 					'conditions_available'		: 'FALSE',
 					'absolute_comparators'		: absolute,
@@ -117,42 +116,45 @@ class AnalysisApplication(symbols.Symbols, utilities.Utilities):
 		if (len_absolute > 0) or ((len_relative > 0) and (len_relative_join > 0)) :
 			logging.debug("We have some conditions: %s", relative)
 			data['conditions_available'] = 'TRUE'
-
 		analysis_query = analysis_queries.AnalysisBackboneQueryTemplate.safe_substitute(data)
 		logging.info("Analysis Query: %s", analysis_query)
 		return analysis_query
 
+	def process_analysis_query_symbol(self, analysis_query, symbol):
+		logging.info("Running query on %s", symbol)
+		#Perform the query per symbol
+		data_parameters = collections.OrderedDict()
+		data_parameters['symbol'] = symbol
+		#Run the query
+		self.get_db().execute(analysis_query, data_parameters)
+		#Get the results
+		analysis_trading_query_results_list = self.get_db().fetchall()
+		for analysis_trading_result in analysis_trading_query_results_list:
+			logging.debug("analysis_trading_result %s", analysis_trading_result)
+			data_parameters = collections.OrderedDict()
+			data_parameters['p_quote'] = analysis_trading_result[0]
+			data_parameters['p_analysis_property'] = analysis_property_id
+			data_list = list(data_parameters.values())
+			#Perform the analysis of the data
+			self.get_db().callproc(analysis_queries.analysis_assignment_storage, data_list)
+			self.database.commit()
+
+	def process_analysis_query(self, analysis_query, symbols=None):
+		if symbols is None:
+			#With query built, apply to the symbols that we have
+			for symbol in self.get_symbols_list():
+				self.process_analysis_query_symbol(analysis_query, symbol)
+		else:
+			self.process_analysis_query_symbol(analysis_query, symbols)
+
+
 	def process_trading_flags(self):
 		logging.info("Process the trading flags")
-		logging.debug("Getting analysis_trading_query")
-		analysis_trading_query = self.database.get_query()
 		for analysis_property in self.get_analysis_properties():
 			logging.debug("Analysis property %s", analysis_property)
 			analysis_property_id, analysis_property_name, analysis_property_type, analysis_property_assigned_value = analysis_property
 			logging.debug("Property: %s : %s - %s", analysis_property_id, analysis_property_name, analysis_property_type)
 			logging.info("Get the analysis conditions for %s : %s", analysis_property_id, analysis_property_name)
 			analysis_query = self.build_analysis_query(analysis_property_id)
-			#With query built, apply to the symbols that we have
-			for symbol in self.get_symbols_list():
-				logging.info("Running query on %s", symbol)
-				#Perform the query per symbol
-				data_parameters = collections.OrderedDict()
-				data_parameters['symbol'] = symbol
-				#Run the query
-				analysis_trading_query.execute(analysis_query, data_parameters)
-				#Get the results
-				analysis_trading_query_results_list = analysis_trading_query.fetchall()
-				for analysis_trading_result in analysis_trading_query_results_list:
-					logging.debug("analysis_trading_result %s", analysis_trading_result)
-					analysis_input_query = self.database.get_query()
-					data_parameters = collections.OrderedDict()
-					data_parameters['p_quote'] = analysis_trading_result[0]
-					data_parameters['p_analysis_property'] = analysis_property_id
-					data_list = list(data_parameters.values())
-					#Perform the analysis of the data
-					analysis_input_query.callproc(analysis_queries.analysis_assignment_storage, data_list)
-					self.database.commit()
-					analysis_input_query.close()
+			self.process_analysis_query(analysis_query, None)
 
-		logging.debug("Closing analysis_trading_query")
-		analysis_trading_query.close()
