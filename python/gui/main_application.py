6b307af2cee2acc3905b4e7696eac0c725959751
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-03-23 23:49:14 +0000

    Added basic combobox. Formatting means it does not display neatly but it does display

--- python/gui/main_application.py
+++ python/gui/main_application.py
@@ -11,5 +11,9 @@ from database import db_connection
 class MainApplicationWindow(wx.Frame):
 	def __init__(self, parent = None, title="Stock Analysis User Interface"):
 		wx.Frame.__init__(self, parent, title = title, size=(1200,800))
+		self.sizer = wx.GridBagSizer(2,4)
+		#Add elements
+		self.symbols = wx.ComboBox(self, size=wx.Size(40, 50), choices = ('A', 'B', 'Z'))
+		#Show
 		self.Show()
 		pass #__init__ functions
