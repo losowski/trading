bbfd36790a2d98e3abe028217ba91dedb05df6fa
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-04-28 23:49:08 +0100

    Added a means to save the symbols and separated out storage of all the data and the schemas

--- database/dump_database.sh
+++ database/dump_database.sh
@@ -5,6 +5,8 @@
 #Script to make entire DBi schema, create etc
 pg_dump -svc --host=localhost -Utrading tradingdb > sql/tradingdb.sql
 #Script to store data only
-pg_dump -va --host=localhost -Utrading tradingdb > sql/tradingdb_data.sql
+pg_dump -va --host=localhost --data-only -Utrading tradingdb > sql/tradingdb_data.sql
 #Compress the data
 tar -cvf sql/tradingdb.tar sql/tradingdb.sql sql/tradingdb_data.sql
+#Exchange and Symbols only
+pg_dump -va --host=localhost -Utrading tradingdb --data-only --table=trading_schema.symbol --table=trading_schema.exchange > sql/tradingdb_symbols.sql
