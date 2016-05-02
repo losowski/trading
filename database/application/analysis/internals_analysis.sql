11b501d39b7c3f947a92b0b1f8ef569346ba84d0
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-05-02 23:57:03 +0100

    Added function handling of UUID. THe actual function now needs to provide one TODO: Add the actual uuid.uuid4 generation

--- database/application/analysis/internals_analysis.sql
+++ database/application/analysis/internals_analysis.sql
@@ -2,11 +2,22 @@
 
 CREATE OR REPLACE FUNCTION trading_schema.pInsAnalysisAssignment(
 	p_quote						trading_schema.quote.id%TYPE,
-	p_analysis_property			trading_schema.analysis_property.id%TYPE
+	p_analysis_property			trading_schema.analysis_property.id%TYPE,
+	p_uuid						trading_schema.reference.reference%TYPE
 ) RETURNS void AS $$
--- DECLARE
+DECLARE
+	v_reference_id				trading_schema.reference.id%TYPE;
 BEGIN
-	-- Delete existing records
+	SELECT
+		id
+	FROM
+		trading_schema.reference
+	WHERE
+		reference = p_uuid
+	;
+	--TODO: NO RESULT HANDLER
+	v_reference_id := 1;
+	--Leave in for now Delete existing records
 	DELETE FROM
 		trading_schema.analysis_assignment_quote
 	WHERE
@@ -19,12 +30,15 @@ BEGIN
 		trading_schema.prediction_input
 		(
 			analysis_property_id,
-			quote_id
+			quote_id,
+			reference_id
 		)
 		VALUES
 		(
 			p_analysis_property,
-			p_quote
+			p_quote,
+			v_reference_id
+	
 		)
 	;
 END;
