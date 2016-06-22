f3126b877d520cd93e2a2a4756395d68e60a4dbc
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-06-22 23:50:52 +0100

    Added tester raw data query

---
+++ database/application/analysis/testing/prediction_tester_input_data.sql
@@ -0,0 +1,21 @@
+﻿SELECT
+	pi.id AS prediction_input_id,
+	s.id AS symbol_id,
+	date_trunc('DAY', q.datestamp) AS startdate,
+	date_trunc('DAY', pi.end_date) AS enddate,
+	pi.end_value,
+	pi.end_diff AS end_diff,
+	ap.analysis_type,
+	ap.price_change,
+	ap.days
+FROM
+	trading_schema.reference r
+	INNER JOIN trading_schema.prediction_input pi ON (r.id = pi.reference_id)
+	INNER JOIN trading_schema.analysis_property ap ON (pi.analysis_property_id = ap.id)
+	INNER JOIN trading_schema.quote q ON (pi.quote_id = q.id)
+	INNER JOIN trading_schema.symbol s ON (q.symbol_id = s.id)
+	LEFT OUTER JOIN trading_schema.prediction_test pt ON (pi.id = pt.prediction_input_id)
+WHERE
+	r.reference = CAST ('e98620c5-e05e-4ae1-a0fc-2e59f7870cee' AS uuid)
+AND
+	pt.prediction_input_id IS NULL
