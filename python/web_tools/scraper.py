bb2ce4733a065767d41f050f45ee3044e5c15886
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-11-19 14:41:32 +0200

    Scraper to use the dryscrape library

--- python/web_tools/scraper.py
+++ python/web_tools/scraper.py
@@ -7,7 +7,6 @@
 
 import logging
 import urllib2
-import xml.etree.ElementTree as xml
 import dryscrape
 import sys
 
@@ -15,8 +14,6 @@ class WebScraper:
 	def __init__(self, url="https://uk.finance.yahoo.com/q/ks?s=GOOG"):
 		self.url = url
 		self.urlnetwork = None
-		self.tree = None
-		self.root = None
 
 	def __del__(self):
 		pass
@@ -27,23 +24,18 @@ class WebScraper:
 		if 'linux' in sys.platform:
 			# start xvfb in case no X is running. Make sure xvfb is installed, otherwise this won't work!
 			dryscrape.start_xvfb()
-		session = dryscrape.Session()
-		session.visit(self.url)
-		session.set_attribute('auto_load_images', False) #Don't load any images
-		self.string = session.body()
-		#print self.string
+
 
 	def run(self):
-		#logging.debug("HTML: %s", self.string)
-		#result = self.buf.read_line().decode("utf-8")
-		#self.tree = xml.fromstring(self.string.decode("utf-8"))
-		self.tree = xml.fromstring(self.string)
-		self.root = self.tree.getroot()
+		self.session = dryscrape.Session()
+		self.session.visit(self.url)
+		self.session.set_attribute('auto_load_images', False) #Don't load any images
 
 	def shutdown(self):
 		pass
 
-	def register(self, var_name, xpath):
+	def register_xpath(self, var_name, xpath):
 		logging.debug("Variable name: %s, XPATH: %s", var_name, xpath)
-		value = self.root.findall(xpath)
+		value = self.session.at_xpath(xpath)
+		logging.debug("Variable: %s, value: %s", var_name, value)
 		setattr(self, var_name, value)
