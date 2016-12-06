37394c65d9aa6b0f771d3eea80de925884f9a85a
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-12-07 01:27:08 +0200

    Json Scraper works 100%

--- python/web_tools/json_scraper.py
+++ python/web_tools/json_scraper.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python
-# Uses the dryscrape library
-# git repo: 	https://github.com/niklasb/dryscrape.git
-# apt-get install qt5-default libqt5webkit5-dev build-essential python-lxml python-pip xvfb
-# pip install dryscrape
-
 
 import logging
 import urllib2
-import httplib
 import json
 
 class JSONScraper:
-	def __init__(self, url="https://query1.finance.yahoo.com/v10/finance/quoteSummary/YHOO?formatted=true&crumb=GfcEejc1sYm&lang=en-GB&region=GB&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=uk.finance.yahoo.com"):
+	def __init__(self, url= "https://query1.finance.yahoo.com/v10/finance/quoteSummary/YHOO?formatted=true&crumb=GfcEejc1sYm&lang=en-GB&region=GB&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=uk.finance.yahoo.com"):
 		self.url = url
 		self.conn = None
 
@@ -19,17 +13,17 @@ class JSONScraper:
 		pass
 
 	def initialise(self):
-		logging.info("URL:%s", self.url)
-		self.conn = httplib.HTTPSConnection("self.url")
-		self.conn.request("GET", "/")
+		self.conn = urllib2.urlopen(self.url)
 
 	def run(self):
-		r1 = conn.getresponse()
-		print r1.status, r1.reason
-		data1 = r1.read()
+		#Read the returned buffer
+		self.data = self.conn.read()
+		pass
 
 	def shutdown(self):
 		pass
 
 	def fetch_json(self):
-		pass
+		logging.debug("JSON data: %s", self.data)
+		json_obj = json.loads(self.data)
+		return json_obj
