84ef7032cf5a926647c66d9f360a14085836b1ea
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-12-08 00:00:03 +0200

    TEMP: using the json scraper instead

--- python/feeds/key_statistics/yahoo_statistics.py
+++ python/feeds/key_statistics/yahoo_statistics.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python
 import logging
-import urllib
-
-from web_tools import scraper
+from web_tools import json_scraper
 
 #NOTE: If you are logged in, this will always hang and fail.
 # LOGOUT to make this work properly
-class YahooStatistics (scraper.WebScraper):
+class YahooStatistics (json_scraper.JSONScraper):
 	def __init__(self, symbol):
-		params = urllib.urlencode({'p': symbol})
-		url = "https://uk.finance.yahoo.com/quote/{0}/key-statistics?%s".format(symbol) % params
-		scraper.WebScraper.__init__(self, url)
+		url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/YHOO?formatted=true&crumb=GfcEejc1sYm&lang=en-GB&region=GB&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=uk.finance.yahoo.com".format(symbol)
+		json_scraper.JSONScraper.__init__(self, url)
 		pass
 
 	def __del__(self):
-		scraper.WebScraper.__del__(self)
+		json_scraper.JSONScraper.__del__(self)
+
+#https://query1.finance.yahoo.com/v10/finance/quoteSummary/YHOO?formatted=true&crumb=GfcEejc1sYm&lang=en-GB&region=GB&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=uk.finance.yahoo.com
 
 	def register(self):
-		self.register_xpath('enterprise_value', '//*[@id="main-0-Quote-Proxy"]/section/div[2]/section/div/section/div[2]/div[1]/div[1]/div/table/tbody/tr[2]/td[2]')
+		json_data = self.fetch_json()
+		logging.info("data: %s", json_data)
+		result = json_data["quoteSummary"]["result"]
+		logging.critical("quote_summary = %s", result)
+		self.register_xpath('enterprise_value', result)
+		#self.register_xpath('enterprise_value', '////*[@id="FIN-MainCanvas"]/div[3]/div/*[@id="main-0-Quote-Proxy"]/section/div[2]/section/div/section/div[2]/div[1]/div[1]/div/table/tbody/tr[2]/td[2]')
 		#self.register_xpath('price_earnings', '//*[@id="quote-header-info"]/div[2]/div[1]/div/span[1]')
 		#self.register_xpath('price_earnings_growth', '')
 		#self.register_xpath('price_sales', '')
@@ -42,3 +46,6 @@ class YahooStatistics (scraper.WebScraper):
 		#self.register_xpath('book_value_per_share', '')
 		#self.register_xpath('operating_cash_flow', '')
 		#self.register_xpath('quarterly_earnings_growth', '')
+
+	def register(self):
+		pass
