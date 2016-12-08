58dc74c774113206212436368765ab3531d66d14
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-12-08 02:30:19 +0200

    Added some yahoo logic Unfortunately the URL might not provide the full stats

--- python/feeds/key_statistics/yahoo_statistics.py
+++ python/feeds/key_statistics/yahoo_statistics.py
@@ -5,6 +5,85 @@ from web_tools import json_scraper
 #NOTE: If you are logged in, this will always hang and fail.
 # LOGOUT to make this work properly
 class YahooStatistics (json_scraper.JSONScraper):
+
+	defaultKeyStatistics = {
+				"enterprise_value"				:("enterpriseValue", "raw"),				#enterprise_value
+				"price_earnings"				:("trailingEps", "raw"),					#price_earnings
+				"price_earnings_growth"			:("pegRatio", "raw"),						#price_earnings_growth
+				"priceToSalesTrailing12Months"	:("priceToSalesTrailing12Months", "raw"),	#price_sales
+				"price_book"					:("priceToBook", "raw"),					#price_book
+				"enterprise_value_revenue"		:("enterpriseToRevenue", "raw"),			#enterprise_value_revenue
+				"enterprise_value_ebitda"		:("enterpriseToEbitda", "raw"),				#enterprise_value_ebitda
+				#profit_margin
+				#operating_margin
+				#return_on_assets
+				#return_on_equity
+				#revenue
+				#revenue_per_share
+				#quarterly_revenue_growth
+				#gross_profit
+				#earnings_before_tax_ebitda
+				#diluted_eps
+				#total_cash
+				#total_cash_per_share
+				#total_debt
+				#total_debt_vs_equity
+				#current_ratio
+				#book_value_per_share
+				#operating_cash_flow
+				#quarterly_earnings_growth
+			}
+
+	financialData = {
+				#enterprise_value
+				#price_earnings
+				#price_earnings_growth
+				#price_sales
+				#price_book
+				#enterprise_value_revenue
+				#enterprise_value_ebitda
+				"profit_margin"					:("profitMargins", "raw"),					#profit_margin
+				"operatingMargins"				:("operatingMargins", "raw"),				#operating_margin
+				"returnOnAssets"				:("returnOnAssets", "raw"),				#return_on_assets
+				"returnOnEquity"				:("returnOnEquity", "raw"),				#return_on_equity
+				"totalRevenue"					:("totalRevenue", "raw"),				#revenue
+				"revenuePerShare"		:("revenuePerShare", "raw"),#revenue_per_share
+				#quarterly_revenue_growth
+				#"grossMargins"		:("grossMargins", "raw"),
+				"gross_profit"		:("grossProfits", "raw"),#gross_profit
+				"ebitda"		:("ebitda", "raw"),#earnings_before_tax_ebitda
+				#diluted_eps
+				"totalCash"		:("totalCash", "raw"),#total_cash
+				"totalCashPerShare"		:("totalCashPerShare", "raw"),#total_cash_per_share
+				"totalDebt"		:("totalDebt", "raw"),#total_debt
+				"debtToEquity"		:("debtToEquity", "raw"),#total_debt_vs_equity
+				"currentRatio"		:("currentRatio", "raw"),#current_ratio
+				#book_value_per_share
+				"operatingCashflow"		:("operatingCashflow", "raw"),#operating_cash_flow
+				#quarterly_earnings_growth
+		}
+
+"!!FinancialData!!"		:("!!FinancialData!!", "raw"),
+
+
+"maxAge"		:("maxAge", "raw"),
+"targetLowPrice"		:("targetLowPrice", "raw"),
+"currentPrice"		:("currentPrice", "raw"),
+"numberOfAnalystOpinions"		:("numberOfAnalystOpinions", "raw"),
+"freeCashflow"		:("freeCashflow", "raw"),
+"ebitdaMargins"		:("ebitdaMargins", "raw"),
+"targetMedianPrice"		:("targetMedianPrice", "raw"),
+"targetHighPrice"		:("targetHighPrice", "raw"),
+"recommendationKey"		:("recommendationKey", "raw"),
+"targetMeanPrice"		:("targetMeanPrice", "raw"),
+"recommendationMean"		:("recommendationMean", "raw"),
+"earningsGrowth"		:("earningsGrowth", "raw"),
+"revenueGrowth"		:("revenueGrowth", "raw"),
+"quickRatio"		:("quickRatio", "raw"),
+""		:("", "raw"),
+
+
+		}
 	def __init__(self, symbol):
 		url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/YHOO?formatted=true&crumb=GfcEejc1sYm&lang=en-GB&region=GB&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=uk.finance.yahoo.com".format(symbol)
 		json_scraper.JSONScraper.__init__(self, url)
@@ -18,8 +97,8 @@ class YahooStatistics (json_scraper.JSONScraper):
 	def register(self):
 		json_data = self.fetch_json()
 		logging.info("data: %s", json_data)
-		result = json_data["quoteSummary"]["result"]
-		logging.critical("quote_summary = %s", result)
+		business_stats = json_data["quoteSummary"]["result"][0]["defaultKeyStatistics"]
+		logging.critical("quote_summary = %s", busine)
 		self.register_xpath('enterprise_value', result)
 		#self.register_xpath('enterprise_value', '////*[@id="FIN-MainCanvas"]/div[3]/div/*[@id="main-0-Quote-Proxy"]/section/div[2]/section/div/section/div[2]/div[1]/div[1]/div/table/tbody/tr[2]/td[2]')
 		#self.register_xpath('price_earnings', '//*[@id="quote-header-info"]/div[2]/div[1]/div/span[1]')
