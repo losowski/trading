9088c53d0096f98c54cc70d8674637f7ca362e07
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2016-12-11 00:15:00 +0200

    JSON scraper scrapes properly

--- python/web_tools/json_scraper.py
+++ python/web_tools/json_scraper.py
@@ -5,9 +5,13 @@ import urllib2
 import json
 
 class JSONScraper:
-	def __init__(self, url= "https://query1.finance.yahoo.com/v10/finance/quoteSummary/YHOO?formatted=true&crumb=GfcEejc1sYm&lang=en-GB&region=GB&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=uk.finance.yahoo.com"):
+	def __init__(self, url , json_mappings):
+		logging.debug("URL: %s", url)
 		self.url = url
 		self.conn = None
+		self.data = None
+		self.json_mappings = json_mappings #JSON-KEY : (database_key, json_field)
+
 
 	def __del__(self):
 		pass
@@ -27,3 +31,27 @@ class JSONScraper:
 		logging.debug("JSON data: %s", self.data)
 		json_obj = json.loads(self.data)
 		return json_obj
+
+	def xpand_json(self, data):
+		if isinstance(data, list):
+			for d in data:
+				logging.debug("LIST: %s\n", d)
+				self.xpand_json(d)
+		elif isinstance (data, dict):
+			for dk,  dv in data.iteritems():
+				logging.debug("DICT DK: %s -> DV: %s", dk, dv)
+				if dk in self.json_mappings:
+					attr, key = self.json_mappings[dk]
+					value = None
+					if key in dv:
+						value = dv[key]
+					#Set the attribute
+					setattr(self, attr, value)
+
+				else:
+					#Not matching parameter
+					self.xpand_json(dv)
+
+	def register(self):
+		logging.debug("importing JSON data")
+		self.xpand_json(self.fetch_json())
