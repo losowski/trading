968e1a24d1a0f32a624268692e692064d44457a4
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-06-05 20:50:41 +0100

    Added script to disable symbols and exchanges

--- symbol.py
+++ symbol.py
@@ -10,9 +10,27 @@ import logging
 from python.database import db_connection
 
 insertExchange = "trading_schema.pinsexchange"
+enableExchange = "trading_schema.pEnableExchange"
+disableExchange = "trading_schema.pDisableExchange"
 insertSymbol = "trading_schema.pInsSymbol"
+enableSymbol = "trading_schema.pEnableSymbol"
+disableSymbol = "trading_schema.pDisableSymbol"
+
+NONE		=	None
+ENABLE		=	'Y'
+DISABLE		=	'N'
+
+def getEnableState(enabled):
+	logger = logging.getLogger('getEnableState')
+	logger.info("Enabled: %s", enabled)
+	state = NONE
+	if enabled in (ENABLE, DISABLE):
+		state = enabled
+	logger.info("State: %s", state)
+	return state
 
 
+# -- Exchange
 def addExchange (db, exchange):
 	logger = logging.getLogger('addExchange')
 	logger.info("Exchange: %s", exchange)
@@ -24,6 +42,25 @@ def addExchange (db, exchange):
 	query.callproc(insertExchange, data_list)
 	update_symbols = query.fetchall()
 
+
+def setExchangeEnabled(db, exchange, state):
+	logger = logging.getLogger('setExchangeEnabled')
+	logger.info("Exchange: %s", exchange)
+	logger.info("State: %s", state)
+	query = db.get_query()
+	data_parameters = collections.OrderedDict()
+	data_parameters['exchange']			= exchange
+	data_list = list(data_parameters.values())
+	if (ENABLE == state):
+		#execute the stored procedure - Enable
+		query.callproc(enableExchange, data_list)
+	elif (DISABLE == state):
+		#execute the stored procedure - Disable
+		query.callproc(disableExchange, data_list)
+	update_symbols = query.fetchall()
+
+
+# -- Symbol
 def addSymbol (db, exchange, symbol, name):
 	logger = logging.getLogger('addSymbol')
 	logger.info("Exchange: %s", exchange)
@@ -39,8 +76,25 @@ def addSymbol (db, exchange, symbol, name):
 	query.callproc(insertSymbol, data_list)
 	update_symbols = query.fetchall()
 
+
+def setSymbolEnabled(db, symbol, state):
+	logger = logging.getLogger('setSymbolEnabled')
+	logger.info("Symbol: %s", symbol)
+	logger.info("State: %s", state)
+	query = db.get_query()
+	data_parameters = collections.OrderedDict()
+	data_parameters['symbol']			= symbol
+	data_list = list(data_parameters.values())
+	if (ENABLE == state):
+		#execute the stored procedure - Enable
+		query.callproc(enableSymbol, data_list)
+	elif (DISABLE == state):
+		#execute the stored procedure - Disable
+		query.callproc(disableSymbol, data_list)
+	update_symbols = query.fetchall()
+
 def main():
-	logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
+	logging.basicConfig(format='%(asctime)s\t%(name)-16s\t%(funcName)-16s\t[%(levelname)-8s] %(message)s', level=logging.DEBUG)
 	logger = logging.getLogger('main')
 	blurb = "Symbol Adder"
 	logger.warning(blurb)
@@ -50,18 +104,28 @@ def main():
 	parser.add_argument('--exchange', dest='exchange', type=str, help='Exchange')
 	parser.add_argument('--symbol', dest='symbol', type=str, help='Stock symbol')
 	parser.add_argument('--name', dest='name', type=str, help='Human readable name')
+	parser.add_argument('--enable', dest='enable', type=str, default='Y', help='Enable')
 	#Get the arguments
 	args = parser.parse_args()
 	logger.info("Args: %s", args)
 
+
 	#Connect to DB
 	db = db_connection.DBConnection()
 	db.connect()
+	#Get change state
+	state = getEnableState(args.enable)
 	# Change DB
 	if (None != args.symbol):
-		addSymbol(db, args.exchange, args.symbol, args.name)
+		if (None != state):
+			setSymbolEnabled(db, args.symbol, state)
+		else:
+			addSymbol(db, args.exchange, args.symbol, args.name)
 	else:
-		addExchange(db, args.exchange)
+		if (None != state):
+			setExchangeEnabled(db, args.exchange, state)
+		else:
+			addExchange(db, args.exchange)
 	#Commit
 	logger.info("Commiting transaction")
 	db.commit()
