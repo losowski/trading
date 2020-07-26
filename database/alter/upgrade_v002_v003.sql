e6fafcb834e7f71e6e0df325daef160307100f28
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-07-26 15:29:13 +0100

    Cleared up some upgrade scripts

--- database/alter/upgrade_v002_v003.sql
+++ database/alter/upgrade_v002_v003.sql
@@ -1,3 +1,9 @@
 -- Add symbol enabled flag
 -- NOTE: Not applied to exchange (as that is < 10 rows) so would not appreciably help
 CREATE INDEX idx_symbol_enabled ON trading_schema.symbol USING btree (enabled);
+
+-- Add "last_update" column to symbol
+ALTER TABLE trading_schema.symbol
+    ADD COLUMN last_update timestamp without time zone;
+
+CREATE INDEX idx_symbol_last_update ON trading_schema.symbol USING btree (last_update);
