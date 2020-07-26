5600287d3288819a20fd174d6edb70770676d8b0
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-07-26 16:21:22 +0100

    New Function for last update of symbol

--- database/alter/upgrade_v002_v003.sql
+++ database/alter/upgrade_v002_v003.sql
@@ -7,3 +7,20 @@ ALTER TABLE trading_schema.symbol
     ADD COLUMN last_update timestamp without time zone;
 
 CREATE INDEX idx_symbol_last_update ON trading_schema.symbol USING btree (last_update);
+
+-- Set the symbol.last_update
+-- Set the last update with NOW (localtimestamp)
+CREATE OR REPLACE FUNCTION trading_schema.pSymbolLastUpdate(
+    p_symbol_id  trading_schema.symbol.id%TYPE
+    ) RETURNS integer AS $$
+BEGIN
+    UPDATE trading_schema.symbol SET last_update = localtimestamp WHERE id = p_symbol_id;
+
+    GET DIAGNOSTICS changed = ROW_COUNT;
+
+    RETURN changed;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Ownership
+ALTER FUNCTION trading_schema.pSymbolLastUpdate OWNER TO trading;
