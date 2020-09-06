ff4d787e8f5d310ce552adadf8bc493acc075bf3
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-09-06 13:10:07 +0100

    Nearly functional single quote update

--- stockQuote.py
+++ stockQuote.py
@@ -4,14 +4,29 @@ Import the Stock Data from Yahoo Finance
 '''
 #import
 import logging
+import argparse
+
+# Yahoo feed data
 from python.historical import yahoo
 
 def main():
-	print ("Quote Importer")
+	blurb = "Quote Importer"
 	logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',level=logging.INFO)
+	logger	=	logging.getLogger('main')
+	logger.warning(blurb)
+	#Parse the arguments
+	parser = argparse.ArgumentParser(description= blurb)
+	#Specify arguments
+	parser.add_argument('--symbol', dest='symbol', type=str, help="Symbol to update")
+	#Parse the arguments
+	args = parser.parse_args()
+	#Process
 	feeds = yahoo.Yahoo()
 	feeds.initialise()
-	feeds.run(True)
+	if args.symbol is not None:
+		feeds.runSymbol(True, args.symbol)
+	else:
+		feeds.run(True)
 	feeds.shutdown()
 	print("Exiting...")
 
