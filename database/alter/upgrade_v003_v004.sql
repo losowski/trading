ee19aa5ecea107acb5ba9d4a743a9412fd1a6f45
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-03-05 23:00:30 +0000

    Added more of the query to categorise

--- database/alter/upgrade_v003_v004.sql
+++ database/alter/upgrade_v003_v004.sql
@@ -8,5 +8,31 @@ CREATE INDEX idx_symbol_category ON trading_schema.symbol USING btree (category)
 
 
 -- Stored Procedures --
-
+-- Procedure to Categorise a single symbol
+CREATE OR REPLACE FUNCTION trading_schema.pCategoriseSymbol(
+	p_symbol		trading_schema.symbol%TYPE,
+	p_date			trading_schema.quote.datestamp%TYPE,
+	p_interval		varchar(10) default '365 days'
+	) RETURNS void AS $$
+DECLARE
+	v_price		numeric := 0;
+BEGIN
+	-- Get minimum price
+	SELECT
+		MIN(open_price)
+	INTO
+		v_price
+	FROM
+		trading_schema.symbol
+	WHERE
+		symbol = p_symbol
+	AND
+		datestamp >= p_date
+	AND
+		datestamp <= p_date -  p_interval::interval
+	;
+	-- Generate category
+	-- Update the table
+END;
+$$ LANGUAGE plpgsql;
 -- Running the procedure
