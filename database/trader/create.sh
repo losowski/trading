a62d07a3a6c728f194dc2e4963656036a788c6ac
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-03-14 18:09:42 +0000

    Fixed the writing data to a file

--- database/trader/create.sh
+++ database/trader/create.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 # Create the database file
 cat create_table/transaction.sql > trader.sql
-cat create_table/futures.sql > trader.sql
-cat create_table/fxsymbols.sql > trader.sql
-cat create_table/forex.sql > trader.sql
+cat create_table/futures.sql >> trader.sql
+cat create_table/fxsymbols.sql >> trader.sql
+cat create_table/forex.sql >> trader.sql
