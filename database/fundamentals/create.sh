cbb5f445799184ac7398e2153751cb84ec98a2e7
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-03-31 22:27:20 +0100

    Added earnings_data

--- database/fundamentals/create.sh
+++ database/fundamentals/create.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 # Create the database file
-cat create_table/earnings.sql > fundamentals.sql
+cat create_table/earnings_data.sql > fundamentals.sql
---
+++ database/fundamentals/create_table/earnings_data.sql
@@ -0,0 +1,58 @@
+-- Table: trading_schema.earnings_data
+CREATE SEQUENCE trading_schema.earnings_data_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE trading_schema.earnings_data_id_seq
+  OWNER TO trading;
+
+
+CREATE TABLE trading_schema.earnings_data
+(
+  id bigint NOT NULL DEFAULT nextval('trading_schema.earnings_data_id_seq'::regclass),
+  symbol_id bigint NOT NULL,
+  datestamp timestamp without time zone NOT NULL,
+  report_type character(1) NOT NULL,
+  -- Data
+  earnings_per_share numeric,
+  total_revenue numeric,
+  cost_of_revenue numeric,
+  gross_profit numeric,
+  total_assets numeric,
+  total_liabilities numeric,
+  total_income_available_shares numeric,
+  common_stock numeric,
+  retained_earnings numeric,
+  total_stockholder_equity numeric,
+  return_on_equity numeric,
+  CONSTRAINT pk_earnings_data_id PRIMARY KEY (id),
+  CONSTRAINT fk_earnings_data_symbol_id FOREIGN KEY (symbol_id)
+      REFERENCES trading_schema.symbol (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT uc_earnings_data_1 UNIQUE (symbol_id, datestamp, report_type),
+  CONSTRAINT ck_earnings_data_report_type CHECK (report_type = ANY (ARRAY['Q'::bpchar, 'Y'::bpchar])) NOT VALID
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE trading_schema.earnings_data
+  OWNER TO trading;
+
+CREATE UNIQUE INDEX idx_earnings_data_id
+  ON trading_schema.earnings_data
+  USING btree
+  (id);
+
+CREATE INDEX idx_datestamp_type
+  ON trading_schema.earnings_data
+  USING btree
+  (datestamp, report_type);
+
+CREATE INDEX idx_symbol_datestamp_report_type
+  ON trading_schema.earnings_data
+  USING btree
+  (symbol_id, datestamp, report_type);
+
+-- Stored Procedures --
