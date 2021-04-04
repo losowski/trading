014dffc6f5bb7ca698009a5e71ab26600fe0df7d
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-04-04 14:38:15 +0100

    Added append pipe

--- database/fundamentals/create.sh
+++ database/fundamentals/create.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 # Create the database file
 cat create_table/earnings_data.sql > fundamentals.sql
-cat create_table/earnings_calcs.sql > fundamentals.sql
+cat create_table/earnings_calcs.sql >> fundamentals.sql
